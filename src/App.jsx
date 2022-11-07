30ce0ce4641d952dba72e4b38de3712f4b3174d7
Davi <df828316@gmail.com>
2022-11-07 16:37:59 -0300

    feat: set a global css colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './global/theme';
 import Routes from './Routes';
 import Provider from './provider';
 
 function App() {
   return (
+    <ThemeProvider theme={ theme }>
       <Provider>
         <BrowserRouter>
           <Routes />
         </BrowserRouter>
       </Provider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/global/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  color: {
+    primario: '#29739c',
+    secundario: '#393939',
+    terciario: '#4a9cee',
+    quaternario: '#838383',
+    textDark: '#001813',
+    textLight: '#F2FFFC',
+  },
+};
+
+export default theme;
